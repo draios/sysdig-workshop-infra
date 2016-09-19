3b06f9108db74dbd85180744be211e8beb763592
Nathen Harvey <nharvey@chef.io>
2016-09-19 10:34:42 -0400

    Fix password auth on RHEL
    * write out new sshd_config
    * update the cloud.cfg
    
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- cookbooks/habitat_workstation/recipes/default.rb
+++ cookbooks/habitat_workstation/recipes/default.rb
@@ -27,28 +27,6 @@ docker_service 'default' do
   action [:create, :start]
 end
 
-template "/etc/selinux/config" do
-  source "selinux-config.erb"
-  mode "0644"
-end
-
-service 'sshd'
-
-template '/etc/ssh/sshd_config' do
-  source 'sshd_config.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :restart, "service[sshd]"
-end
-
-template '/home/ec2-user/sshd_config' do
-  source 'sshd_config.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
-  notifies :restart, "service[sshd]"
-end
 
 user 'chef' do
   comment 'ChefDK User'
@@ -64,12 +42,21 @@ sudo 'chef' do
   template "chef-sudoer.erb"
 end
 
-template '/etc/cloud/cloud.cfg' do
-  template 'cloud.cfg.erb'
-  owner 'root'
-  group 'root'
-  mode '0644'
+service 'sshd'
+
+if node['platform_family'] == 'rhel'
+  template '/etc/ssh/sshd_config' do
+    source 'sshd_config.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    notifies :restart, "service[sshd]"
+  end
+
+  template '/etc/cloud/cloud.cfg' do
+    source 'cloud.cfg.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
 end
-# execute "sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config" do
-#   notifies :restart, "service[sshd]"
-# end
